0777da812843cdc3840b6d545377bdb02d1d925c
William Santos <william.santos86@fatec.sp.gov.br>
2025-07-25 23:52:39 -0300

    update: 01-numbers/scripts.js

--- 01-numbers/scripts.js
+++ 01-numbers/scripts.js
@@ -1,12 +1,12 @@
-console.log(1)
-console.log(-15)
-console.log(+89)
+console.log(1);
+console.log(-15);
+console.log(+89);
 
 console.log("Arithmetic Operators ")
 
-console.log(1 + 89)
-console.log(100 - 99)
-console.log(100 * 9)
-console.log(50 / 2)
-console.log(50 % 2)
-console.log(2 ** 3)
+console.log(1 + 89);
+console.log(100 - 99);
+console.log(100 * 9);
+console.log(50 / 2);
+console.log(50 % 2);
+console.log(2 ** 3);
