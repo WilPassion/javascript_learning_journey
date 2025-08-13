b3809e4156af7f6ce58484fa7d4b2aab1d2ee985
William Santos <william.santos86@fatec.sp.gov.br>
2025-08-13 12:16:21 -0300

    feat: add aula JSON

---
+++ 13-JSON/scripts.js
@@ -0,0 +1,11 @@
+// JSON = JavaScript Object Notation
+// Utilizado para comunicação entre serviços, ex: back end <-> front end
+// Basicamente um tipo de dado padronizado, que lembra muito os objetos do JavaScript
+
+
+let pessoa = {
+    "name": "Matheus",
+    "age": 29,
+    "position": "Developer",
+    "languages": ["PHP", "JavaScript", "Python"]
+}
